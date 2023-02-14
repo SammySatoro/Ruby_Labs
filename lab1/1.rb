8050c690ca3bdd1130546f2838dd89eca71470c5
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2023-02-14 03:06:10 +0300

    2.1

--- lab1/1.rb
+++ lab1/1.rb
@@ -1,16 +1,17 @@
-# 1
-puts('Hello World')
+def ex_1
+  # 1.1
+  puts('Hello World')
 
-# 2
-puts 'What\'s your name?'
-name = gets.chop
-puts "Hello, #{name} \nWhat is your favorite programming language?"
-answer = gets.chop.downcase
-case answer
-when 'ruby'
-  puts 'Ma boy!'
-when 'php'
-  puts "Did I ever tell you what the definition of insanity is?'
+  # 1.2
+  puts 'What\'s your name?'
+  name = gets.chop
+  puts "Hello, #{name} \nWhat is your favorite programming language?"
+  answer = gets.chop.downcase
+  case answer
+  when 'ruby'
+    puts 'Ma boy!'
+  when 'php'
+    puts "Did I ever tell you what the definition of insanity is?'
   Insanity is doing the exact... same f**king thing... over and over again expecting... shit to change...
   That. Is. Crazy. The first time somebody told me that, I dunno, I thought they were bullshitting me, so, I shot him.
   The thing is... He was right. And then I started seeing, everywhere I looked, everywhere I looked all these f**king pricks,
@@ -20,24 +21,57 @@ when 'php'
   F**k you! Okay? F**k you!... It's okay, man. I'm gonna chill, hermano. I'm gonna chill... The thing is...
   Alright, the thing is I killed you once already... and it's not like I am f**king crazy. It's okay...
   It's like water under the bridge. Did I ever tell you that ruby will be your favorite soon?"
-when 'python'
-  puts "Ligma balls before you can comprehend ruby."
-when 'c++'
-  puts "All of these voices inside of my head
+  when 'python'
+    puts "Ligma balls before you can comprehend ruby."
+  when 'c++'
+    puts "All of these voices inside of my head
   Blinding my sight in a curtain of red
   Frustration is getting bigger
   Bang, bang, bang, pull my Devil Trigger
   ...
   You're up to ruby upgrade, didn't you?"
-else
-  puts 'It will be ruby soon'
+  else
+    puts 'It will be ruby soon'
+  end
+
+  # 1.3
+  puts 'Enter any ruby code:'
+  input_1 = gets.chop
+  eval input_1
+
+  puts 'Enter any Linux command:'
+  input2 = `#{gets.chop}`
+  puts input2
 end
 
-# 3
-puts 'Enter any ruby code:'
-input_1 = gets.chop
-eval input_1
+# 2
+# Вариант № 4
+# Метод 1 Найти количество четных чисел, не взаимно простых с данным
+# Метод 2 Найти максимальную цифры числа, не делящуюся на 3
+# Метод 3 Найти произведение максимального числа, не взаимно простого
+# с данным, не делящегося на наименьший делитель исходно числа, и
+# суммы цифр числа, меньших 5
 
-puts 'Enter any Linux command:'
-input2 = `#{gets.chop}`
-puts input2
+# 2.1
+
+def ex_2
+  def gcd(a, b)
+    if a == 0 or b == 0 then
+      a + b
+    else
+      next_a = a > b ? a % b : a
+      next_b = a <= b ? b % a : b
+      gcd(next_a, next_b)
+    end
+  end
+
+  def count_of_non_coprime(value, arr)
+    counter = 0
+    arr.each do |i|
+      counter += (gcd(value, i) != 1 && i % 2 == 0) ? 1 : 0
+    end
+    counter
+  end
+
+  puts count_of_non_coprime(16, [*1..16])
+end
