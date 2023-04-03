0b569f4a1a3bfb112076f5c954b98e06079bf8ae
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2023-04-04 00:18:09 +0300

    Add files via upload

---
+++ lab2/student_regexes.rb
@@ -0,0 +1,10 @@
+module StudentRegexes
+  ID_REGEX = /^[0-9]+$/
+  PHONE_REGEX = /^(\+7|8)?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{2}[\s.-]?\d{2}$/
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
+  GIT_REGEX = /^(?:https?:\/\/)?(?:www\.)?github\.com\/([a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,38})\/?$/
+  TELEGRAM_REGEX = /^@[a-zA-Z0-9_]{5,32}$/
+  NAME_PART_REGEX = /^([A-Z][a-z]*|[А-Я][а-я]*)/
+  FROM_STRING_REGEX = /[ ,]+/
+
+end
