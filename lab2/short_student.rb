6157bbe32ee1583654e78c0a184c96b65c22afac
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2023-04-04 00:53:18 +0300

    Update short_student.rb

--- lab2/short_student.rb
+++ lab2/short_student.rb
@@ -1,23 +1,23 @@
-require_relative 'student'
+require_relative 'super_student'
 
-class StudentShort
-  attr_reader :id, :short_name, :git, :contact
+class StudentShort < SuperStudent
 
-  def initialize(student_object)
-    raise ArgumentError, '\'student_object\' must be Student type' unless student_object.is_a? Student
+  public_class_method :new
 
-    @id = student_object.id
-    @short_name = student_object.initials
-    @git = student_object.git
-    @contact = student_object.contact_data.values[0]
+  attr_accessor :full_name, :contact
+
+  def StudentShort.from_object(student_object)
+    new(student_object.id, student_object.get_info)
   end
 
-  def self.from_string(string)
-    new(Student.from_string(string))
+  def initialize(id, str)
+    values_str = str.split(FROM_STRING_REGEX)
+    self.id = id
+    self.full_name = values_str[0..2].join(' ')
+    self.git = values_str[3] == '-' ? nil : values_str[3]
+    self.contact = values_str[4] == '-' ? nil : values_str[4]
   end
 
-  def to_s
-    "ID: #{@id} Short name: #{@short_name} GIT: #{@git} Contact: #{@contact}"
-  end
 end
+
 
