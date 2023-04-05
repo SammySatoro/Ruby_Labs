a02f6fad9debe0b559d63738c53954e51322eaca
SammySatoro <ivzhenko2014@gmail.com>
2023-04-06 02:46:49 +0300

    created DataList class

---
+++ lab2/task3/data_list.rb
@@ -0,0 +1,46 @@
+
+class DataList
+
+  protected
+
+  attr_writer :data
+
+  public
+
+  attr_reader :names, :data
+
+  def initialize(data_array)
+    self.list = data_array
+    @selected = []
+  end
+
+  def list=(data_array)
+    @list = []
+    data_array.each_with_index do |data, index|
+      @list << [index, data]
+    end
+    nil
+  end
+
+  def add_to_selection(number)
+    @selected << number
+    @selected.flatten!
+    @selected.uniq!
+    nil
+  end
+
+  def get_selected
+    selection = []
+    @selected.each do |number|
+      result = (@list.find { |pair| pair[0] == number })
+
+      selection << result[1].id
+    end
+    selection
+  end
+
+  def clear_selected
+    @selected = []
+    nil
+  end
+end
